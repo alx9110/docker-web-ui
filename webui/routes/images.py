540fb4d01f7f3b8ec53adcac11d08993b3133c87
alx9110 <alx9110@yandex.ru>
2017-12-17 01:59:43 +0300

    some changes

--- webui/routes/images.py
+++ webui/routes/images.py
@@ -1,6 +1,5 @@
 """ General route """
 from flask import render_template, Blueprint
-from ..common import docker
 import subprocess
 
 
@@ -10,6 +9,4 @@ view = Blueprint('images', __name__)
 @view.route('/images', methods=['GET'])
 def images():
     """ Index page """
-    ps = subprocess.check_output('docker ps -a', shell=True).decode().split('\n')[1:]
-    ps_obj = [docker.Container(raw) for raw in ps if raw]
-    return render_template('images.html', ps=ps_obj)
+    return render_template('images.html')
