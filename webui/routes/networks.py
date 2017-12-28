f0f9090e4ee5048a93fbcea082f7a8663e8b1bab
alx9110 <alx9110@yandex.ru>
2017-12-28 23:49:54 +0300

    network create/delete/inspect added

--- webui/routes/networks.py
+++ webui/routes/networks.py
@@ -1,5 +1,5 @@
 """ General route """
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, flash, request, redirect, jsonify
 import docker
 
 
@@ -8,7 +8,34 @@ view = Blueprint('networks', __name__)
 
 @view.route('/networks', methods=['GET'])
 def networks():
-    """ Index page """
+    """ Network list """
     client = docker.from_env()
     nets = client.networks.list()
     return render_template('networks.html', nets=nets)
+
+
+@view.route('/networks/create', methods=['GET'])
+def network_create():
+    """ Network create """
+    client = docker.from_env()
+    name = request.args.get('net_name')
+    client.networks.create(name)
+    flash('Success, network <{name}> has been created'.format(name=name))
+    return redirect('/networks')
+
+
+@view.route('/networks/delete/<name>', methods=['GET'])
+def network_remove(name=None):
+    """ Remove network """
+    client = docker.from_env()
+    client.networks.get(name).remove()
+    flash('network <{name}> has been remove'.format(name=name))
+    return redirect('/networks')
+
+
+@view.route('/networks/inspect/<name>', methods=['GET'])
+def network_inspect(name=None):
+    """ Remove network """
+    client = docker.from_env()
+    inspect = client.networks.get(name).attrs
+    return jsonify(inspect)
