b1f76581b7db019b059264eecf8342cd7fec8263
alx9110 <alx9110@yandex.ru>
2018-01-25 00:35:07 +0300

    fixed

--- tests.py
+++
@@ -1,9 +0,0 @@
-""" Unit tests """
-from unittest import TestCase
-
-
-class DockerTestCase(TestCase):
-    """ Class for testing Docker SDK """
-    def test_container_list(self):
-        """ client.container.list() testing """
-        self.assertEqual('Hello', 'Hello')
--- webui/routes/dockerfile.py
+++ webui/routes/dockerfile.py
@@ -3,13 +3,6 @@ import os
 from flask import request, render_template, Blueprint, session
 import docker
 import time
-from rq import Queue
-from redis import Redis
-from tasks import count_words_at_url
-
-
-redis_conn = Redis()
-queue = Queue(connection=redis_conn)
 
 
 view = Blueprint('dockerfile', __name__)
@@ -17,25 +10,4 @@ view = Blueprint('dockerfile', __name__)
 
 @view.route('/dockerfile', methods=['GET'])
 def dashboard():
-    """ Index page """
-    client = docker.from_env()
-    plh = \
-    """
-    FROM ubuntu:14.04
-    MAINTAINER Alexander Telkov <alx9110@yandex.ru>
-    RUN apt-get update
-    RUN apt-get install -y nginx
-    RUN echo 'Hi, I am in your container'
-            >/usr/share/nginx/html/index.html
-    EXPOSE 80
-    """
-    job = queue.enqueue(count_words_at_url, 'http://ya.ru')
-    user = session.get('login', None)
-    return str(job.id)
-
-
-@view.route('/status/<id>')
-def upload_file(id):
-    """ Dockerfile upload """
-    job = queue.fetch_job(id)
-    return str(job.result)
+    return 'ok'
