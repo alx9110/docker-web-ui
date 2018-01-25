0e16977801654711aa5e1855462c7882cfa65a6d
alx9110 <alx9110@yandex.ru>
2018-01-26 00:33:35 +0300

    Added rq task

--- webui/routes/dockerfile.py
+++ webui/routes/dockerfile.py
@@ -3,11 +3,19 @@ import os
 from flask import request, render_template, Blueprint, session
 import docker
 import time
+from webui.tasks import count_words_at_url
+from rq import Queue
+from redis import Redis
 
 
 view = Blueprint('dockerfile', __name__)
 
 
 @view.route('/dockerfile', methods=['GET'])
-def dashboard():
-    return 'ok'
+def autobuild():
+    """ Clone and build some git repo with Dockerfile """
+    redis_conn = Redis()
+    queue = Queue(connection=redis_conn)
+    job = queue.enqueue(count_words_at_url, 'http://ya.ru')
+    time.sleep(4)
+    return job.status
--- webui/tasks.py
+++ webui/tasks.py
@@ -1,8 +1,6 @@
 import requests
+import subprocess
 
 
-def count_words_at_url(url):
-    resp = requests.get(url)
-    return url 
-
-print('Tasks')
+def count_words_at_url(url):    
+    return subprocess.call('git clone https://github.com/alx9110/docker-auto-build.git')
