69d942716d958a98ff5a7d3f8b617c2d501a7ef7
alx9110 <alx9110@yandex.ru>
2018-02-02 23:02:07 +0300

    try fixed build

--- webui/tasks.py
+++ webui/tasks.py
@@ -5,16 +5,9 @@ from subprocess import Popen, PIPE
 def build_from_git(url):
     """ Task for auto-build from GIT repo """
     tmp_name = url.replace('.git', '').split('/')[-1]
+    cmd = """git clone {0} /tmp/user@{1}&&cd /tmp/user@{1}&&docker build -t user@{1} ."""
     # Stage1: git clone
-    clone = Popen('git clone {0} /tmp/{1}'.format(url, tmp_name),
-                shell=True, stdout=PIPE, universal_newlines=True
-               )
-    # Stage2: docker build
-    build = Popen('docker build -t {0} /tmp/{0}'.format(tmp_name),
-                shell=True, stdout=PIPE, universal_newlines=True
-               )
-    # Stage3: clean workdir
-    clean = Popen('sudo rm -R /tmp/{0}'.format(tmp_name),
+    clone = Popen(cmd.format(url, tmp_name),
                 shell=True, stdout=PIPE, universal_newlines=True
                )
     return clone.stdout.read() + build.stdout.read() + clean.stdout.read()
